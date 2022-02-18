5ab8a2a42f98b711e27b751507103b0213ee72c3
florian <florian@Workstation>
2022-02-18 10:24:28 +0100

    Implementing rand and clap

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-use std::env;
+use clap::Parser;
+use rand::Rng;
 
-fn usage() {
-    eprintln!("Usage: passg [ OPTIONS ] [ length ] \n\n\
-	       -C Use capitalized letters in the password \n\
-	       -c Don't use capitalized letters in the password \n\
-	       -N Use numbers in the passowrd \n\
-	       -n Don't use numbers in the password \n\
-	       -S Use special symbols in the password \n\
-	       -s Don't use special symbols in the password \n\n\
-	       The default is -CNS with a length of 32.");
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    capitalized: bool,
+
+    #[clap(short, long)]
+    numbers: bool,
+
+    #[clap(short, long)]
+    special: bool,
+
+    #[clap(short, long)]
+    length: u32,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
+    let mut rng = rand::thread_rng();
 
-    match args.len() {
-	_ => {
-	    usage();
-	}
+    const DEFAULT_CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz";
+    const CAPITALIZED_CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const NUMBER_CHARSET: &[u8] = b"0123456789";
+    const SPECIAL_CHARSET: &[u8] = b")(*&^%$#@!~\"\'";
+
+    let mut charset: Vec<u8> = DEFAULT_CHARSET.to_vec();
+
+    if args.capitalized {
+        charset.append(&mut CAPITALIZED_CHARSET.to_vec());
     }
+
+    if args.numbers {
+        charset.append(&mut NUMBER_CHARSET.to_vec());
+    }
+
+    if args.special {
+        charset.append(&mut SPECIAL_CHARSET.to_vec());
+    }
+
+    let password: String = (0..args.length).map(|_| {
+	let index = rng.gen_range(0..charset.len());
+	charset[index] as char
+    }).collect();
+
+    println!("{}", password);
 }
